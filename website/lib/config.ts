a935404f9080480cbd9fb8e4612fd962e14ce81e
Victor Grenu <victor.grenu@external.engie.com>
2026-04-05 12:48:09 +0200

    Remove configuration file for base paths and data URLs from the project

--- website/lib/config.ts
+++
@@ -1,5 +0,0 @@
-// Get the base path for the application
-export const basePath = process.env.NODE_ENV === "production" ? "/MAMIP" : "";
-
-// Helper to get data URLs
-export const getDataUrl = (path: string) => `${basePath}/data/${path}`;
